77d0979e5121e9de64674ff69b5f579bcb7f4642
Jay A <justynadusei@gmail.com>
2020-03-31 12:52:55 +0000

    Reordered methods

--- Swift100Project2/ViewController.swift
+++ Swift100Project2/ViewController.swift
@@ -25,44 +25,43 @@ class ViewController: UIViewController {
         
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
         
-        func askQuestion(action: UIAlertAction!) {
-            
-            //
-            
-            countries.shuffle()
-            
-            // Setting image for buttons with countries in an array
-            button1.setImage(UIImage(named: countries[0]), for: .normal)
-            button2.setImage(UIImage(named: countries[1]), for: .normal)
-            button3.setImage(UIImage(named: countries[2]), for: .normal)
-            
-            // Putting correct country title in Nav bar
-            
-            title = countries[correctAnswer].uppercased()
-            
-            // Choosing the right answer
-            correctAnswer = Int.random(in: 0...2)
-            
-            
-            // Setting border for buttons
-            button1.layer.borderWidth = 1
-            button2.layer.borderWidth = 1
-            button3.layer.borderWidth = 1
-            
-            // Setting border color for buttons
-            button1.layer.borderColor = UIColor.white.cgColor
-            button2.layer.borderColor = UIColor.white.cgColor
-            button3.layer.borderColor = UIColor.white.cgColor
-            
-            // Setting corner radius for the buttons
-            button1.layer.cornerRadius = 10
-            button2.layer.cornerRadius = 10
-            button3.layer.cornerRadius = 10
-            
-        }
+        // Setting border for buttons
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
         
-        askQuestion(action: nil)
+        // Setting border color for buttons
+        button1.layer.borderColor = UIColor.white.cgColor
+        button2.layer.borderColor = UIColor.white.cgColor
+        button3.layer.borderColor = UIColor.white.cgColor
+        
+        // Setting corner radius for the buttons
+        button1.layer.cornerRadius = 10
+        button2.layer.cornerRadius = 10
+        button3.layer.cornerRadius = 10
+        
+        askQuestion()
 
+    }
+    
+    func askQuestion(action: UIAlertAction! = nil) {
+        
+        // Shuffle the order of countries
+        countries.shuffle()
+        
+        // Setting image for buttons with countries in an array
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        // Putting correct country title in Nav bar
+        title = countries[correctAnswer].uppercased()
+        
+        // Choosing the right answer
+        correctAnswer = Int.random(in: 0...2)
+        
+        
+        
     }
     
     @IBAction func buttonTouched(_ sender: UIButton) {
